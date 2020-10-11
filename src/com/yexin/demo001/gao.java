9005e6b6c7ac417e3d8110109f073f0b3ddd3aa0
MiBookPro <MiBookPro@DESKTOP-FMEOGGT.mshome.net>
2020-10-11 22:11:52 +0800

    D方法

--- src/com/yexin/demo001/gao.java
+++ src/com/yexin/demo001/gao.java
@@ -8,6 +8,9 @@ public class gao {
 	private void C() {
 		System.out.println("C方法");
 	}
+	private void D() {
+		System.out.println("D方法");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println();
