4e5b30433160e2d02ae10e1872dc2eff31ae98cf
MiBookPro <MiBookPro@DESKTOP-FMEOGGT.mshome.net>
2020-10-11 21:14:36 +0800

    gaohq 提交

--- src/com/yexin/demo001/gao.java
+++ src/com/yexin/demo001/gao.java
@@ -2,11 +2,20 @@ package com.yexin.demo001;
 
 public class gao {
 
+	public static String name;
+	
+	private static void myName() {
+		name = "凌妍妍";
+		System.out.println(name);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println();
 			System.out.println();
-			System.out.println("gaohuiqishigeshabiqqqqq444");
+			System.out.println("我是真的爱你呀 亲爱的，和你一起写代码感觉真好，一起学习啊~~~");
+			//this is static method, change by gaohq
+			myName();
 	}
 
 }
