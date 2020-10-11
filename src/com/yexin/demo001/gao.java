c096da44dadcff88a8b1c70d6f77d21a9413a8ac
MiBookPro <MiBookPro@DESKTOP-FMEOGGT.mshome.net>
2020-10-11 22:57:03 +0800

    憨憨是个大傻子2

--- src/com/yexin/demo001/gao.java
+++ src/com/yexin/demo001/gao.java
@@ -23,6 +23,10 @@ public class gao {
 		System.out.println("G方法");
 	}
 	
+	private void h() {
+		System.out.println("G方法");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println();
@@ -33,6 +37,7 @@ public class gao {
 			System.out.println("不爱憨憨");
 			System.out.println("2222");
 			System.out.println("憨憨是个大傻子");
+			System.out.println("憨憨是个大傻子2");
 	}
 
 }
