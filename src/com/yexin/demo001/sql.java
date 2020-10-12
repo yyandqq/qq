aa24625c970f05266119f5d7832d43dc317803ec
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-12 10:56:57 +0800

    在sql中添加AA方法

--- src/com/yexin/demo001/sql.java
+++ src/com/yexin/demo001/sql.java
@@ -1,7 +1,9 @@
 package com.yexin.demo001;
 
 public class sql {
-
+	private void AA(){
+		System.out.print("AA·½·¨");
+		}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ahha");
