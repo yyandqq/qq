784b04622e3c639af7738e662a12e720f1c6850c
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-12 12:28:19 +0800

    tijiao

--- src/com/yexin/demo001/sql.java
+++ src/com/yexin/demo001/sql.java
@@ -1,6 +1,10 @@
 package com.yexin.demo001;
 
 public class sql {
+	
+	private void BB(){
+		System.out.print("BB·½·¨");
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
