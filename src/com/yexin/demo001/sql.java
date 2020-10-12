4e09df4bd7a427c7d3f80fbd3d085b9d4aa15f8d
yexin147 <72687842+yexin147@users.noreply.github.com>
2020-10-12 14:33:45 +0800

    4444

--- src/com/yexin/demo001/sql.java
+++ src/com/yexin/demo001/sql.java
@@ -3,16 +3,17 @@ package com.yexin.demo001;
 public class sql {
 	
 	private void BB(){
-		System.out.print("BB·½·¨");
+		System.out.print("BB方法");
 		}
 	
 	private void WWW(){
-		System.out.print("WWW·½·¨");
+		System.out.print("WWW方法");
+		System.out.print("4444方法");
 		}
 	
 	
 	private void FF(){
-		System.out.print("FF·½·¨");
+		System.out.print("FF方法");
 		System.out.print("5555555");
 
 		}
