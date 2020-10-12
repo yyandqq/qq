017359c2f33bd43a6ce84248f5d64ccb3b36e763
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-12 10:24:25 +0800

    强制更新

--- src/com/yexin/demo001/gao.java
+++ src/com/yexin/demo001/gao.java
@@ -2,47 +2,10 @@ package com.yexin.demo001;
 
 public class gao {
 	
-	private void B() {
-		System.out.println("方法B");
-	}
-
 	private void A() {
 		System.out.println("A方法");
 	}
-	private void C() {
-		System.out.println("C方法");
-	}
-	private void D() {
-		System.out.println("D方法");
-	}
-	private void F() {
-		System.out.println("F方法");
-	}
-	
-	private void g() {
-		System.out.println("G方法");
-	}
-	
-	private void h() {
-		System.out.println("G方法");
-	}
-	
-	private void o() {
-		System.out.println("o方法");
-	}
-	private void K() {
-		System.out.println("K方法");
-	}
-	private void I() {
-		System.out.println("I方法");
-		System.out.println("tian");
-		System.out.println("tian2");
-	}
-	private void P() {
-		System.out.println("P方法");
 
-	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println();
