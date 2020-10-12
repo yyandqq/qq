84cef1bcfde97bf2e979d9ea982cdf0495b3bd66
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-12 10:27:13 +0800

    sts C方法

--- src/com/yexin/demo001/gao.java
+++ src/com/yexin/demo001/gao.java
@@ -5,6 +5,10 @@ public class gao {
 	private void A() {
 		System.out.println("A方法");
 	}
+	
+	private void C() {
+		System.out.println("C方法");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
