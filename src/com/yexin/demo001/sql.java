c28fbbcf701f0fc068710643df48a7e0e21fbe66
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-12 12:31:40 +0800

    sql中添加www方法

--- src/com/yexin/demo001/sql.java
+++ src/com/yexin/demo001/sql.java
@@ -5,6 +5,10 @@ public class sql {
 	private void BB(){
 		System.out.print("BB方法");
 		}
+	
+	private void WWW(){
+		System.out.print("WWW方法");
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
