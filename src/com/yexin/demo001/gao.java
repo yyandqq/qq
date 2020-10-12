93caa2782bba2c5679ddb2cacf54cb77071bc2c7
yexin147 <72687842+yexin147@users.noreply.github.com>
2020-10-12 12:43:51 +0800

    gao中删除所有的打印

--- src/com/yexin/demo001/gao.java
+++ src/com/yexin/demo001/gao.java
@@ -17,19 +17,7 @@ public class gao {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println();
-			System.out.println();
-			System.out.println("gaohuiqishigeshabiqqqqq444");
-			System.err.println("gaohuiqiershazi ");
-			System.out.println("神经");
-			System.out.println("不爱憨憨");
-			System.out.println("2222");
-			System.out.println("研研是个小仙女");
-			System.out.println("憨憨是个大傻子");
-			System.out.println("不爱憨憨2");
-			System.out.println("我想睡觉");
-			System.out.println("憨憨是个大傻子2");
-			System.out.println("憨憨是个大傻子3");
+			
 	}
 
 }
