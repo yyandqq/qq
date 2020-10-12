9bd3ee3cb0bd159a6dc9e836fef5d89f48bc7ec5
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-12 10:38:12 +0800

    去掉所有的方法

--- src/com/yexin/demo001/gao.java
+++ src/com/yexin/demo001/gao.java
@@ -2,16 +2,7 @@ package com.yexin.demo001;
 
 public class gao {
 	
-	private void A() {
-		System.out.println("A方法");
-	}
-	
-	private void C() {
-		System.out.println("C方法");
-	}
-	private void B() {
-		System.out.println("B方法");
-	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
