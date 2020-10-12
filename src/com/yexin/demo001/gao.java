012102ced7046fe67aabe0189d599511b47bec1e
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-12 14:17:50 +0800

    qqqq

---
+++ src/com/yexin/demo001/gao.java
@@ -0,0 +1,28 @@
+package com.yexin.demo001;
+
+public class gao {
+	
+	private void AA(){
+	System.out.print("AA方法");
+	}
+	
+	private void AB(){
+	System.out.print("AB方法");
+	System.out.print("QQ方法");
+	}
+	
+	private void SS(){
+	System.out.print("SS方法");
+		System.out.print("SSDD方法");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println();
+			System.out.println("AA");
+			
+			
+	}
+
+}
