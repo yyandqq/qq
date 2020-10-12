da261a4c6ed17f7c9d4c9912e73487b09af6ce13
MiBookPro <MiBookPro@DESKTOP-FMEOGGT.mshome.net>
2020-10-13 00:23:11 +0800

    333

--- src/com/yexin/demo001/sql.java
+++ src/com/yexin/demo001/sql.java
@@ -8,7 +8,10 @@ public class sql {
 		}
 	private void B(){
 		System.out.print("BB22222222222222222222方法");
-		System.out.print("BBbbbbbb方法");		}
+		System.out.print("BBbbbbbb方法");	
+		System.out.print("BBbbbbbb33333方法");	
+	}
+	
 
 
 	public static void main(String[] args) {
