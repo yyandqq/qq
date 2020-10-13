90f6a30a663db66defb78b49da42d3cb3f023382
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-13 09:04:57 +0800

    1111

--- src/com/yexin/demo001/sql.java
+++ src/com/yexin/demo001/sql.java
@@ -2,21 +2,11 @@ package com.yexin.demo001;
 
 public class sql {
 	
-	private void BB(){
-		System.out.print("BBbbbbbb方法");
-		System.out.print("BBbbbbbb方法");
-		System.out.print("BBbbbbbb1111方法");
-		System.out.print("BBbbbbbb999方法");
-		System.out.println("更改BB方法");
-		System.out.println("我比莎莎来的早");
+	private void A(){
+
 		}
 	private void B(){
-		System.out.print("BB22222222222222222222方法");
-		System.out.print("BBbbbbbb方法");	
-		System.out.print("BBbbbbbb33333方法");
-		System.out.print("BBbbbbbb33333方法");
-		System.out.print("BBbbbbbb33333方法");
-		System.out.print("莎莎来晚了");
+
 	}
 	
 
