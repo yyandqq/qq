3d49933cbe77f78b7ecaddd1d451f5d58a2befe7
Yexin <Yexin@DESKTOP-KKCU9HC>
2020-10-15 20:09:47 +0800

    加上 ggg

---
+++ src/com/yexin/test/Test001.java
@@ -0,0 +1,14 @@
+package com.yexin.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test001 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/com/yexin/test/sqlTest.java
@@ -0,0 +1,14 @@
+package com.yexin.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class sqlTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
